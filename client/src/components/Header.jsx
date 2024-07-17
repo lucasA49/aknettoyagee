92fcd9fe5c7a9d7498c9c369c4079e5dccd868a1
Lucasa49 <lucasaksu@gmail.com>
2024-07-17 17:34:29 +0200

    factorisation &  final du header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,18 +2,20 @@ import "../styles/Header.css";
 import { NavLink } from "react-router-dom";
 import imgBanner from "../assets/images/background-header.jpg";
 import logo from "../assets/images/aknettoyage.png";
+import tel from "../assets/images/telephone.jpg";
 
 function Header() {
   return (
     <header>
       <nav id="top-nav">
         <img className="logo" src={logo} alt="" />
+
         <div id="contact-nav">
+          <img src={tel} alt="" />
           <p className="numero-tel">0756936273</p>
-          <NavLink className="bouton-header" to="/">
-            <p>CONTACTEZ NOUS</p>{" "}
-          </NavLink>
+          <NavLink to="/">CONTACTEZ NOUS</NavLink>
         </div>
+
       </nav>
 
       {/* EN ATTENTE DE ROOT POUR LE BOUTON !! */}
@@ -24,7 +26,7 @@ function Header() {
       </nav>
 
       <section id="banner-image">
-        <img className="image-background" src={imgBanner} alt="" />
+        <img src={imgBanner} alt="" />
         <div className="blue-banner" />
         <hgroup>
           <h3>Lavage intérieur à partir de 30€</h3>
