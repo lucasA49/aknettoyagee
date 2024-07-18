1280949f57e22415b0abd5c4ab169200b42e8e36
Lucasa49 <lucasaksu@gmail.com>
2024-07-18 15:07:56 +0200

    mise en place du body de accueil

--- client/src/components/BodyAccueil.jsx
+++ client/src/components/BodyAccueil.jsx
@@ -1,23 +1,34 @@
-import canap from "../assets/images/lavcanapé.png"
-import exterieur from "../assets/images/lavexterieur.png"
-import interieur from "../assets/images/lavinterieur.png"
-import "../styles/BodyAccueil.css"
+import canap from "../assets/images/lavcanapé.png";
+import exterieur from "../assets/images/lavexterieur.png";
+import interieur from "../assets/images/lavinterieur.png";
+import pressingsiege from "../assets/images/pressingsiege.png"
+import "../styles/BodyAccueil.css";
 
 function BodyAccueil() {
   return (
-    
-   <div className="contenaire-body">
-    <div className="titre-image-container">
-      <h2 className="titre-body">AK NETTOYAGE à votre services ses compétences sur Cholet et aux alentours </h2>
-      <div className="image-body-container">
-        <img className="interieur " src={interieur} alt="" />
-        <img className="exterieur" src={exterieur}alt="" />
-        <img className="canap"src={canap} alt="" />
-      </div>
-    </div>
-    </div>
-    // <h3>Quelles sont les prestations de nettoyage proposées par l’entreprise  ?</h3>
-
+    <>
+      <div className="contenaire-body">
+        <div className="titre-image-container">
+          <h2 className="titre-body">
+            AK NETTOYAGE à votre service, ses compétences sur Cholet et aux
+            alentours
+          </h2>
+          <div className="image-body-container">
+            <img className="interieur" src={interieur} alt="" />
+            <img className="exterieur" src={exterieur} alt="" />
+            <img className="canap" src={canap} alt="" />
+            </div>
+          </div>
+        </div>
+    <div className="contenaire-presentation">
+              <h3 className="titre-prestations">
+                Quelles sont les prestations de nettoyage proposées par
+                l'entreprise ?
+              </h3>
+              <img className="img-texte-accueil"src={pressingsiege} alt="" />
+              </div>
+   
+    </>
   );
 }
 
